9f4c6c32a2a8cdc2973d198234d20c58e6edcaf4
Nicola Bernardi <84014354+nBernardi05@users.noreply.github.com>
2023-05-02 20:07:06 +0200

    Add files via upload

---
+++ 4apr2022/doc/fatt.cpp
@@ -0,0 +1,19 @@
+//fattoriale
+#include <iostream>
+//creato da Nicola Bernardi, caricato su https://portalenicola.it/didattica/cpp
+using namespace std;
+int fattoriale(int);
+int main(){
+    int n;
+    cout<<"Dammi il numero di cui vuoi calcolare il fattoriale: ";
+    cin>>n;
+    cout<<fattoriale(n)<<endl;
+}//creato da Nicola Bernardi, caricato su https://portalenicola.it/didattica/cpp
+int fattoriale(int n){
+    int fatt=1;
+    while(n>1){
+        fatt=fatt*n;
+        n--;
+    }//creato da Nicola Bernardi, caricato su https://portalenicola.it/didattica/cpp
+    return fatt;
+}
